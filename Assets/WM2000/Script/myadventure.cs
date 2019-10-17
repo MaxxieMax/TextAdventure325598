d6fab9e1803fc4a03f33af5e7287e6390b5af3fc
MaxxieMax <325598@student.mboutreht.nl>
2019-10-17 15:27:08 +0200

    einde game

--- Assets/WM2000/Script/myadventure.cs
+++ Assets/WM2000/Script/myadventure.cs
@@ -24,6 +24,27 @@ public class myadventure : MonoBehaviour
         dood1,
         //politie
         zolder,
+        zolder2,
+        //keuze3
+        keuze3,
+        //blijft zitten
+        kloppen,
+        probeertOpenTeDoen,
+        Dood2,
+        //zolderraam
+        dak,
+        lichten,
+        //keuze4
+        keuze4,
+        //dood3
+        valt,
+        dood3,
+        //einde
+        agenten,
+        roept,
+        beneden,
+        einde,
+
 
         
 
@@ -42,7 +63,7 @@ public class myadventure : MonoBehaviour
         Terminal.WriteLine("Welkom bij De engste nacht van je leven");
         Terminal.WriteLine("Dit is gebaseerd op een waargebeurd    verhaal.");
         Terminal.WriteLine("je moet na elke de vraag verder typen.");        
-        Terminal.WriteLine("Typ start om te beginnen....");
+        Terminal.WriteLine("Type start om te beginnen....");
     }
 
     // Update is called once per frame
@@ -52,7 +73,7 @@ public class myadventure : MonoBehaviour
     }
 
     void OnUserInput(string input)
-    {
+     {
         switch (currentState)
         {
 
@@ -68,7 +89,7 @@ public class myadventure : MonoBehaviour
             
             //shortcut
         else if (input == "save")
-            gevonden();
+            keuze3();
             
         
         else
@@ -138,7 +159,7 @@ public class myadventure : MonoBehaviour
 
         if(input == "verder")
             {
-                slapen();
+                wakker();
             }
             else
         {
@@ -190,7 +211,7 @@ public class myadventure : MonoBehaviour
             }
             else if (input == "politie bellen")
             {
-                //politie();
+                zolder();
             }
             else
         {
@@ -221,7 +242,7 @@ public class myadventure : MonoBehaviour
             Terminal.WriteLine("je moet verder intypen");
         }
             break;
-        //beging einde 1
+        //begin dood1
          case States.dood1:
 
         if(input == "begin")
@@ -230,21 +251,214 @@ public class myadventure : MonoBehaviour
             }
             else
         {
-            Terminal.WriteLine("typ begin om naar het begin te gaan");
+            Terminal.WriteLine("type begin om naar het begin te gaan");
+        }
+            break;
+        //begin zolder
+         case States.zolder:
+
+        if(input == "verder")
+            {
+                zolder2();
+            }
+            else
+        {
+            Terminal.WriteLine("je moet verder intypen");
+        }
+            break;
+        //begin zolder2
+         case States.zolder2:
+
+        if(input == "verder")
+            {
+                keuze3();
+            }
+            else
+        {
+            Terminal.WriteLine("je moet verder intypen");
+        }
+            break;
+        //begin keuze3
+        case States.keuze3:
+
+            if (input == "door het raam klimmen")
+            {
+                dak();
+            }
+            else if (input == "blijven zitten")
+            {
+                kloppen();
+            }
+            else
+        {
+            Terminal.WriteLine("je moet: door het raam klimmen of      blijven zitten intypen.");
+        }
+            break;
+        //begin kloppen
+         case States.kloppen:
+
+        if(input == "verder")
+            {
+                probeertOpenTeDoen();
+            }
+            else
+        {
+            Terminal.WriteLine("je moet verder intypen");
+        }
+            break;
+        //begin probeertOpenTeDoen
+         case States.probeertOpenTeDoen:
+
+        if(input == "verder")
+            {
+                dood2();
+            }
+            else
+        {
+            Terminal.WriteLine("je moet verder intypen");
+        }
+            break;
+        //begin dood2
+         case States.Dood2:
+
+        if(input == "begin")
+            {
+                StartIntro();
+            }
+            else
+        {
+            Terminal.WriteLine("type begin om naar het begin te gaan");
+        }
+            break;
+        //begin dak
+         case States.dak:
+
+        if(input == "verder")
+            {
+                lichten();
+            }
+            else
+        {
+            Terminal.WriteLine("je moet verder intypen");
+        }
+            break;
+        //begin lichten
+         case States.lichten:
+
+        if(input == "verder")
+            {
+                keuze4();
+            }
+            else
+        {
+            Terminal.WriteLine("je moet verder intypen");
+        }
+            break;
+        //begin keuze4
+        case States.keuze4:
+
+            if (input == "naar beneden")
+            {
+                valt();
+            }
+            else if (input == "blijf zitten")
+            {
+                agenten();
+            }
+            else
+        {
+            Terminal.WriteLine("je moet: naar beneden of blijf zitten  intypen.");
+        }
+            break;
+        //begin dood3
+        //begin valt
+         case States.valt:
+
+        if(input == "verder")
+            {
+                dood3();
+            }
+            else
+        {
+            Terminal.WriteLine("je moet verder intypen");
+        }
+            break;
+        //begin dood3
+         case States.dood3:
+
+        if(input == "begin")
+            {
+                StartIntro();
+            }
+            else
+        {
+            Terminal.WriteLine("type begin om naar het begin te gaan");
+        }
+            break;
+        //begin agentenn
+         case States.agenten:
+
+        if(input == "verder")
+            {
+                roept();
+            }
+            else
+        {
+            Terminal.WriteLine("je moet verder intypen");
+        }
+            break;
+        //begin roept
+         case States.roept:
+
+        if(input == "verder")
+            {
+                beneden();
+            }
+            else
+        {
+            Terminal.WriteLine("je moet verder intypen");
+        }
+            break;
+        //begin beneden
+         case States.beneden:
+
+        if(input == "verder")
+            {
+                einde();
+            }
+            else
+        {
+            Terminal.WriteLine("je moet verder intypen");
+        }
+            break;
+        //begin EINDE
+         case States.einde:
+
+        if(input == "begin")
+            {
+                StartIntro();
+            }
+            else
+        {
+            Terminal.WriteLine("type begin om naar het begin te gaan");
         }
             break;
 
 
+
+
       
+
+      //probeertOpenTeDoen
     }
     //start
     void StartIntro()
     {
         Terminal.ClearScreen();
-        Terminal.WriteLine("het is vrijdag avond,");
-        Terminal.WriteLine("je bent helemaal alleen thuis.");
-        Terminal.WriteLine("je ouders en broertjes zijn weg.");
-        Terminal.WriteLine("na het eten begin je lekker te gamen.");
+        Terminal.WriteLine("Het is vrijdag avond,");
+        Terminal.WriteLine("Je bent helemaal alleen thuis.");
+        Terminal.WriteLine("Je ouders en broertjes zijn weg.");
+        Terminal.WriteLine("Na het eten begin je lekker te gamen.");
 
         currentState = States.intro;
     }
@@ -281,8 +495,7 @@ public class myadventure : MonoBehaviour
         Terminal.WriteLine("je doet voorzichtig de deur open,");
         Terminal.WriteLine("de man kijkt je een beetje vaag aan.");
         Terminal.WriteLine("je vraagt of de man weg wilt gaan.");
-        Terminal.WriteLine("met verbazing loopt de man langzaam weg....");
-       // Terminal.WriteLine("met een verwarde gedachten ga je weer  slapen.");
+        Terminal.WriteLine("Met verbazing loopt de man langzaam weg....");
 
         currentState = States.deelja;
     }
@@ -290,8 +503,8 @@ public class myadventure : MonoBehaviour
     void deelnee()
     {
         Terminal.ClearScreen();
-        Terminal.WriteLine("het zou wel een vogel zijn, dacht je.");
-        Terminal.WriteLine("met die gedachten ging je weer         rustig slapen");
+        Terminal.WriteLine("Het zou wel een vogel zijn, dacht je.");
+        Terminal.WriteLine("Met die gedachten ging je weer         rustig slapen");
         Terminal.WriteLine("maar later zou je anders denken.....");
 
         currentState = States.deelnee;
@@ -365,13 +578,173 @@ public class myadventure : MonoBehaviour
         void dood1()
     {
         Terminal.ClearScreen();
-        Terminal.WriteLine("je bent na de avond nooit meer terug gezien...");
+        Terminal.WriteLine("Je bent na de avond nooit meer terug   gezien...");
         
 
         Terminal.WriteLine("GAME OVER!");
 
         currentState = States.dood1;
     }
+
+                //zolder
+        void zolder()
+    {
+        Terminal.ClearScreen();
+        Terminal.WriteLine("Terwijl de man de andere kamers checkt sluip je snel naar het zolderluik.");
+        Terminal.WriteLine("Je doet het zolderluik open, maar de   man hoort dat!");
+        Terminal.WriteLine("Snel klim je de trap op en doe je het  zolderluik dicht.");
+        Terminal.WriteLine("Net optijd!");
+
+        currentState = States.zolder;
+    }
+                    //zolder
+        void zolder2()
+    {
+        Terminal.ClearScreen();
+        Terminal.WriteLine("Terwijl je op zolder zit probeer je de politie te bellen.");
+        Terminal.WriteLine("Je hebt net genoeg bereik om ze te     bellen!");
+        Terminal.WriteLine("Ze komen er zo snel mogelijk aan!");
+
+        currentState = States.zolder2;
+    }
+        //keuzen3
+        void keuze3()
+    {
+        Terminal.ClearScreen();
+        Terminal.WriteLine("Je ziet dat het zolderaam open staat.");
+        Terminal.WriteLine("Je bent net groot genoeg om er doorheen te klimmen.");
+        Terminal.WriteLine("Je kan ook binnen blijven zitten.");
+        Terminal.WriteLine("Ga je blijven zitten of ga je door het raam heen klimmen?");
+
+        currentState = States.keuze3;
+    }
+        //kloppen
+        void kloppen()
+    {
+        Terminal.ClearScreen();
+        Terminal.WriteLine("De man begint opeens heel hard op het   zolderluik te kloppen!");
+        Terminal.WriteLine("Hij stopt gelukig even, hij heeft het  volgensmij opgegeven.");
+
+        currentState = States.kloppen;
+    }
+        //probeertOpenTeDoen
+        void probeertOpenTeDoen()
+    {
+        Terminal.ClearScreen();
+        Terminal.WriteLine("Opeens begint de man heel hard aan het zolderluik te trekken!");
+        Terminal.WriteLine("Ik probeer het luik zo goed mogelijk   dicht te houden.");
+        Terminal.WriteLine("Zo ga ik het niet lang vol houden.");
+
+        currentState = States.probeertOpenTeDoen;
+    }
+            //dood2
+        void dood2()
+    {
+        Terminal.ClearScreen();
+        Terminal.WriteLine("Na een tijdje krijgt de man het luik   toch open!");
+        Terminal.WriteLine("De man klimt naar boven...");
+        Terminal.WriteLine("Je bent na de avond nooit meer terug   gezien...");
+        Terminal.WriteLine("GAME OVER!");
+
+        currentState = States.Dood2;
+    }
+            //dak
+        void dak()
+    {
+        Terminal.ClearScreen();
+        Terminal.WriteLine("Je klimt snel door het krappezolderraam");
+        Terminal.WriteLine("het past gelukkig net!");
+        Terminal.WriteLine("Het raam is net te klein voor de man om er doorheen te komen.");
+        Terminal.WriteLine("Terwijl je op het dak zit hoor je in de verte de politie al!");
+
+        currentState = States.dak;
+    }
+               //lichten
+        void lichten()
+    {
+        Terminal.ClearScreen();
+        Terminal.WriteLine("Je ziet ook al snel de lichten van de  politewagen");
+        Terminal.WriteLine("alleen hoe kom ik van het dak af?");
+
+        currentState = States.lichten;
+    }
+                   //keuze4
+        void keuze4()
+    {
+        Terminal.ClearScreen();
+        Terminal.WriteLine("Blijf je op het dak zitten?");
+        Terminal.WriteLine("Of probeer je er af te gaan?");
+        Terminal.WriteLine("Het is wel 3 verdiepingen hoog...");
+        Terminal.WriteLine("Ga je naar beneden of blijf je zitten?");
+
+        currentState = States.keuze4;
+    }
+                   //valt
+        void valt()
+    {
+        Terminal.ClearScreen();
+        Terminal.WriteLine("Je probeert naar beneden te klimmen...");
+        Terminal.WriteLine("Omdat het donker is weet je niet goed  waar je je voeten neer moet zetten.");
+        Terminal.WriteLine("Bij de 2e stap gaat het al fout!");
+        Terminal.WriteLine("Je valt hard naar beneden....");
+
+        currentState = States.valt;
+    }
+            //dood3
+        void dood3()
+    {
+        Terminal.ClearScreen();
+        Terminal.WriteLine("Je valt hard op de grond.");
+        Terminal.WriteLine("Er gaat opeens een heftige pijnscheut  door je been heen...");
+        Terminal.WriteLine("Door de heftige pijn val je            buitenwesten.");
+        Terminal.WriteLine("Je bent na de avond nooit meer terug   gezien...");
+        Terminal.WriteLine("GAME OVER!");
+
+        currentState = States.dood3;
+    }
+                //agenten
+        void agenten()
+    {
+        Terminal.ClearScreen();
+        Terminal.WriteLine("Je ziet dat de de politiewagen steeds dichterbij komen.");
+        Terminal.WriteLine("Je ziet dat er 2 politie agenten naar de deur lopen.");
+
+        currentState = States.agenten;
+    }
+                //roept
+        void roept()
+    {
+        Terminal.ClearScreen();
+        Terminal.WriteLine("Je fluistert naar de agenten maar ze   horen je niet!");
+        Terminal.WriteLine("Uitijndelijk schreeuw je naar de       agenten.");
+        Terminal.WriteLine("Ze kijken je eerst een beetje vreemd   aan,");
+         Terminal.WriteLine("maar uitijndelijk proberen ze je te    helpen.");
+
+        currentState = States.roept;
+    }
+                //beneden
+        void beneden()
+    {
+        Terminal.ClearScreen();
+        Terminal.WriteLine("De agenten helpen je veilig naar       beneden.");
+        Terminal.WriteLine("Er stont toevallig een ladder tegen het huis aan.");
+
+        currentState = States.beneden;
+    }
+            //einde
+        void einde()
+    {
+        Terminal.ClearScreen();
+        Terminal.WriteLine("Eenmaal veilig beneden aangekomen,");
+        Terminal.WriteLine("Vertel je de agenten het hele veraal.");
+        Terminal.WriteLine("Dierect stormen de agenten het huis    binnen en");
+        Terminal.WriteLine("arresteren ze de man!!");
+        Terminal.WriteLine("GEFELICITEERD!");
+        Terminal.WriteLine("JE HEBT DE GAME UITGESPEELD!");
+
+        currentState = States.einde;
+    }
+
 
 
 } 
